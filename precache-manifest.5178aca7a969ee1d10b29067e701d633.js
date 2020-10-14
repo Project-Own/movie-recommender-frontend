d3eb72f4e21eb676daeeea76d8ddaa4526f0c814
Sahas Prajapati <b10prajapati@gmail.com>
2020-10-14 14:41:00 +0545

    Updates

---
+++ precache-manifest.5178aca7a969ee1d10b29067e701d633.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02a3131a660b0d50989e1656a6750ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "543e69006fcf9f760306",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "739922067bf9b881cef9",
+    "url": "/static/js/2.8fc50bbf.chunk.js"
+  },
+  {
+    "revision": "f270aa4cc0bdb77fa27a196abca673aa",
+    "url": "/static/js/2.8fc50bbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "543e69006fcf9f760306",
+    "url": "/static/js/main.e13059e8.chunk.js"
+  },
+  {
+    "revision": "f6ba3f2635e6e7dd351f",
+    "url": "/static/js/runtime-main.a99cdb19.js"
+  }
+]);
