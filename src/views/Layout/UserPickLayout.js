a34b4d3506cf761fc6b334ab1c718fda771202cc
rohitjoey <roht.praz@gmail.com>
2020-11-04 14:41:03 +0545

    frontview layout or user pick layout added to layout

---
+++ src/views/Layout/UserPickLayout.js
@@ -0,0 +1,31 @@
+import { Grid} from '@material-ui/core';
+import Header from './components/frontPage/Header';
+import Contents from './components/frontPage/Contents'
+
+
+
+const App = () =>{
+  return (
+    <Grid container direction='column'>
+      <Grid item > 
+        <Header/>
+      </Grid>
+      <Grid item align='center'>
+        Yeta nirjal ko search bar rakhne
+      </Grid>
+      <Grid item container>
+        <Grid item xs={2} sm={2} md={2}/>
+        <Grid item xs={10} sm={8} md={8}>
+          <Contents/>
+        </Grid>
+        <Grid item  xs={2} sm={2} md={2}/>
+
+      </Grid>
+     
+    </Grid>
+    
+    
+  );
+}
+
+export default App;
