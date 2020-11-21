9e9de27bb5a1a1b1dc23fce83d490c9352849f05
flyingDutchman01 <nirjalprajapati@gmail.com>
2020-11-21 20:10:49 +0545

    yarn deleted

--- src/components/Recommender/Recommender.js
+++ src/components/Recommender/Recommender.js
@@ -5,9 +5,12 @@ import FormLabel from "@material-ui/core/FormLabel";
 import Paper from "@material-ui/core/Paper";
 
 import data from "./data";
-import ButtonBase from "@material-ui/core/CardActionArea";
-import { pink } from "@material-ui/core/colors";
+import MovieCard from "../SearchComponent/MovieCard";
 
+import Button from '@material-ui/core/Button';
+
+
+const API_ADDRESS = "https://www.omdbapi.com/?apikey=e4c29baa&t=";
 const useStyles = makeStyles((theme) => ({
     image: {
         maxWidth: "200px",
@@ -22,10 +25,10 @@ const useStyles = makeStyles((theme) => ({
         // maxWidth: "200px",
         maxHeight: "20px",
         minWidth: "150px",
-        minHeight: "20px",
-        borderRadius: 10,
+        minHeight: "30px",
+        borderRadius: 5,
         "&:hover": {
-            color: "red"
+            color: "pink"
         },
         marginBottom: 10,
         marginTop: 10
@@ -38,29 +41,47 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
-function shoot() {
-    console.log("I am shoot");
-}
+
 export default function Recommender() {
-    const [movie, recommendMovie] = React.useState("guarded bayou");
+    const [movie, setMovie] = React.useState("guarded bayou");
+    const [movie1, setMovie1] = React.useState("");
+
     const classes = useStyles();
 
-    return ( <
+    const searchMovie = (movie) => {
+        if (movie !== "") {
+            fetch(`${API_ADDRESS} + ${movie}`)
+                .then((response) => response.json())
+                .then((json) => {
+                    if (json.Response !== "False") {
+                        // setState({ ...state, movie: json });
+                        console.log(json);
+                        setMovie1(json);
+                        // setQuery("");
+                    }
+                })
+                .catch((error) => alert(error.message));
+        }
+    };
+
+    return ( < >
+        <
         Grid container className = { classes.root }
         spacing = { 2 } >
         <
         Grid item xs = { 12 } >
         <
         Paper style = {
-            { width: 300, height: 20 } } >
+            { width: 300, height: 20 }
+        } >
         <
         Grid item >
         <
         FormLabel > Recommendation
-        for { movie } < /FormLabel> <
-        /Grid> <
-        /Paper> <
-        /Grid>
+        for { movie } < /FormLabel>  <
+        /Grid >  <
+        /Paper>  <
+        /Grid >
 
         <
         Grid item xs = { 12 } >
@@ -70,10 +91,12 @@ export default function Recommender() {
             data.map((movie) => ( <
                 Grid item >
                 <
-                ButtonBase className = { classes.buttonbasestyle }
-                onClick = { shoot } >
-
-                { movie.title }
+                Button className = { classes.buttonbasestyle }
+                variant = "outlined"
+                color = "primary"
+                onClick = {
+                    () => { searchMovie(movie.title) } } > { movie.title } <
+                /Button>
 
                 {
                     /* <img
@@ -81,13 +104,16 @@ export default function Recommender() {
                                       src={"https://image.tmdb.org/t/p/w185" + movie.posterPath}
                                       alt="rohitjoey"
                                     ></img> */
-                } <
-                /ButtonBase> <
+                }
+
+                <
                 /Grid>
             ))
         } <
-        /Grid> <
-        /Grid> <
-        /Grid>
-    );
-}
+        /Grid>  <
+        /Grid >  <
+        /Grid>  { < MovieCard movie = { movie1 }
+            />} <
+            />
+        );
+    }
