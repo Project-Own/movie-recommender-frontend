f4fed3362bc22bd66114d4ae84f8e1e79bacc347
Nirajan9860 <prajapatinirajan0@gmail.com>
2021-02-18 19:43:46 +0545

    loading

--- src/views/Layout/Profile.js
+++ src/views/Layout/Profile.js
@@ -11,10 +11,17 @@ import { useDispatch, useSelector } from "react-redux";
 import Cell from "../../components/Recommender/Cell";
 import { loadUser } from "../../features/Auth/loadUser";
 import { selectToken, selectUser } from "../../features/Auth/registerSlice";
+import { Skeleton } from "@material-ui/lab";
 
 const preferredMoviesURI =
   "https://vae-login.herokuapp.com/api/profile/preferred_movies";
-const Profile = () => {
+const Profile = (props) => {
+  const {
+    loading=false,
+    height = 300,
+    breadth = 200,
+    
+  } = props;
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
   const token = useSelector(selectToken);
@@ -46,20 +53,31 @@ const Profile = () => {
         <Typography>{user?.name ?? "Unknown"}</Typography>
         <Typography>{user?.email ?? "Unknown"}</Typography>
         <Grid container>
-          {typeof movieList !== "undefined"
-            ? movieList.map((movie, index) => (
-                <Grid item>
-                  <Cell
-                    key={index}
-                    data={movie}
-                    height={300}
-                    width={200}
-                    style={{ margin: 10 }}
-                    searchMovie={() => {}}
-                  />
-                </Grid>
+          {loading || typeof movieList == "undefined" 
+            ? new Array(5)
+              .fill(0)
+              .map((value, index) => (
+                <Skeleton
+                  key={index}
+                  animation="wave"
+                  height={height}
+                  width={breadth}
+                  variant="rect"
+                  style={{ margin: 10 }}
+                />
               ))
-            : null}
+            : movieList?.map((movie, index) => (
+              <Grid item>
+                <Cell
+                  key={index}
+                  data={movie}
+                  height={300}
+                  width={200}
+                  style={{ margin: 10 }}
+                  searchMovie={() => {}}
+                />
+              </Grid>
+            ))}
         </Grid>
       </Container>
     </>
